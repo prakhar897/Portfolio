2aad0ce34190488f808f656f845e213b945eec29
Prakhar Gupta <prakhar897@gmail.com>
2023-12-28 14:08:28 +0530

    x

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -8,33 +8,33 @@ const Blog = () => {
 			<Navbar />
 			<ul>
 				{/* <li>
-					<Link to="blog/playing-with-llm-vulnerabilities">
+					<Link to="/blog/playing-with-llm-vulnerabilities">
 						Playing with LLM Vulnerabilites <b>[30 Dec 2023]</b>
 					</Link>
 				</li> */}
 				<li>
-					<Link to="blog/moving-away-from-substack">
+					<Link to="/blog/moving-away-from-substack">
 						Moving Away from Substack <b>[17 Nov 2023]</b>
 					</Link>
 				</li>
 
 				<li>
-					<Link to="blog/real-world-stats-for-bootstrapping">
+					<Link to="/blog/real-world-stats-for-bootstrapping">
 						Real World Stats for IndieHacking <b>[19 Sep 2023]</b>
 					</Link>
 				</li>
 				<li>
-					<Link to="blog/todo-list-that-actually-works">
+					<Link to="/blog/todo-list-that-actually-works">
 						A TODO List that Actually Works <b>[07 Sep 2023]</b>
 					</Link>
 				</li>
 				<li>
-					<Link to="blog/envisioning-a-future-with-gpt">
+					<Link to="/blog/envisioning-a-future-with-gpt">
 						Envisioning A Future With GPT <b>[15 Apr 2023]</b>
 					</Link>
 				</li>
 				<li>
-					<Link to="blog/chatgpt-in-diffudle">
+					<Link to="/blog/chatgpt-in-diffudle">
 						Using ChatGPT to automate my AI Art Game
 						<b>[21 Dec 2022]</b>
 					</Link>
