a409c52690d15d589c99e00c62b12bb6491ebf69
Prakhar Gupta <prakhar897@gmail.com>
2024-01-23 12:21:49 +0530

    x

--- src/App.js
+++ src/App.js
@@ -1,102 +1,64 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog/Blog";
-import Projects from "./pages/Projects/Projects";
+import Projects from "./pages/Projects";
 import MovingAwayFromSubstack from "./pages/Blog/MovingAwayFromSubstack/MovingAwayFromSubstack";
 import RealWorldStatsForBootstrapping from "./pages/Blog/RealWorldStatsForBootstrapping/RealWorldStatsForBootstrapping";
 import ToDoListThatWorks from "./pages/Blog/ToDoListThatActuallyWorks/ToDoListThatActuallyWorks";
 import EnvisioningFutureWithGPT from "./pages/Blog/EnvisioningFutureWithGPT/EnvisioningFutureWithGPT";
 import ChatGPTInDiffudle from "./pages/Blog/ChatGPTInDiffudle/ChatGPTInDiffudle";
-import LiQuoridor from "./pages/Projects/LiQuoridor";
-import HNCommentsDrawer from "./pages/Projects/HNCommentsDrawer";
-import ColdMessageAI from "./pages/Projects/ColdMessageAI";
-import WorkaroundGPT from "./pages/Projects/WorkaroundGPT";
-import Diffudle from "./pages/Projects/Diffudle";
-import HeadlineHero from "./pages/Projects/HeadlineHero";
-import NodeSaas from "./pages/Projects/NodeSaas";
 import HNCommentsDrawerDemo from "./pages/Misc/HNCommentsDrawerDemo";
-import Resurrecting from "./pages/Projects/Resurrecting";
 import "./App.css";
 import HireMe from "./pages/HireMe";
-import SkillTree from "./pages/SkillTree";
 import LLMVulnerabilityPlayground from "./pages/Blog/PlaygroundForLLMVulnerabilites/LLMVulnerabilityPlayground";
 import Bookmarks from "./pages/Misc/Bookmarks";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/blog" element={<Blog />} />
-                    <Route path="/projects" element={<Projects />} />
-                    <Route path="/hireme" element={<HireMe />} />
-                    <Route path="/skillTree" element={<SkillTree />} />
+	return (
+		<Router>
+			<div className="App">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/blog" element={<Blog />} />
+					<Route path="/projects" element={<Projects />} />
+					<Route path="/hireme" element={<HireMe />} />
 
-                    {/* Routes for individual blog posts */}
-                    <Route
-                        path="/blog/playing-with-llm-vulnerabilities"
-                        element={<LLMVulnerabilityPlayground />}
-                    />
-                    <Route
-                        path="/blog/moving-away-from-substack"
-                        element={<MovingAwayFromSubstack />}
-                    />
-                    <Route
-                        path="/blog/real-world-stats-for-bootstrapping"
-                        element={<RealWorldStatsForBootstrapping />}
-                    />
-                    <Route
-                        path="/blog/todo-list-that-actually-works"
-                        element={<ToDoListThatWorks />}
-                    />
-                    <Route
-                        path="/blog/envisioning-a-future-with-gpt"
-                        element={<EnvisioningFutureWithGPT />}
-                    />
-                    <Route
-                        path="/blog/chatgpt-in-diffudle"
-                        element={<ChatGPTInDiffudle />}
-                    />
+					{/* Routes for individual blog posts */}
+					<Route
+						path="/blog/playing-with-llm-vulnerabilities"
+						element={<LLMVulnerabilityPlayground />}
+					/>
+					<Route
+						path="/blog/moving-away-from-substack"
+						element={<MovingAwayFromSubstack />}
+					/>
+					<Route
+						path="/blog/real-world-stats-for-bootstrapping"
+						element={<RealWorldStatsForBootstrapping />}
+					/>
+					<Route
+						path="/blog/todo-list-that-actually-works"
+						element={<ToDoListThatWorks />}
+					/>
+					<Route
+						path="/blog/envisioning-a-future-with-gpt"
+						element={<EnvisioningFutureWithGPT />}
+					/>
+					<Route
+						path="/blog/chatgpt-in-diffudle"
+						element={<ChatGPTInDiffudle />}
+					/>
 
-                    {/* Routes for individual projects */}
+					<Route
+						path="/misc/hn-comments-drawer-demo"
+						element={<HNCommentsDrawerDemo />}
+					/>
 
-                    <Route
-                        path="/projects/resurrecting"
-                        element={<Resurrecting />}
-                    />
-                    <Route
-                        path="/projects/li-quoridor"
-                        element={<LiQuoridor />}
-                    />
-                    <Route
-                        path="/projects/hn-comments-drawer"
-                        element={<HNCommentsDrawer />}
-                    />
-                    <Route
-                        path="/projects/cold-message-ai"
-                        element={<ColdMessageAI />}
-                    />
-                    <Route
-                        path="/projects/workaround-gpt"
-                        element={<WorkaroundGPT />}
-                    />
-                    <Route path="/projects/diffudle" element={<Diffudle />} />
-                    <Route
-                        path="/projects/headline-hero"
-                        element={<HeadlineHero />}
-                    />
-                    <Route path="/projects/node-Saas" element={<NodeSaas />} />
-                    <Route
-                        path="/misc/hn-comments-drawer-demo"
-                        element={<HNCommentsDrawerDemo />}
-                    />
-
-                    <Route path="/misc/bookmarks" element={<Bookmarks />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+					<Route path="/misc/bookmarks" element={<Bookmarks />} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,49 +2,59 @@ import React from "react";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
-    return (
-        <>
-            <Navbar />
-            <p>
-                Hi, I'm Prakhar, a "Full Stack Developer" who focuses on
-                building and deploying web apps. In the initial years, I learnt
-                how to create systems that are highly scalable, can handle heavy
-                load and are highly resilient. After years of sticking just to
-                the tech side of things, I started taking bigger roles which
-                included coming up with product initiatives to bring in more
-                business for the product.
-                <br />
-                <br />
-                This site is a brain dump of everything I've learned from being
-                a software developer for the last 7 years. Check out the
-                <a href="./hireme"> Hire Me</a> page if you want to know more
-                about my work or to explore the possibility of working together.
-            </p>
-            <h2 className="text-center">Profiles</h2>
-            <div className="flex justify-center align-items">
-                <ul>
-                    <li>
-                        <a href="https://github.com/prakhar897"> Github </a>
-                    </li>
-                    <li>
-                        <a href="https://linkedin.com/in/prakhar897">
-                            LinkedIn
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://stackoverflow.com/users/7293918/p-gupta">
-                            StackOverflow
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://codeforces.com/profile/prakhar897">
-                            Codeforces
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<Navbar />
+
+			<a
+				href="https://tubesearch.prakgupta.com/"
+				target="_blank"
+				rel="noreferrer"
+				className="font-orbitron border py-4 rounded-3xl text-center grid place-items-center"
+			>
+				{" "}
+				Latest work = Tubesearch{" "}
+			</a>
+			<p className="mt-12">
+				Hi, I'm Prakhar, a developer who focuses on building web apps.
+				In the initial years, I learnt how to create systems that are
+				highly scalable, can handle heavy load and are highly resilient.
+				After years of sticking just to the tech side of things, I
+				started taking bigger roles which included coming up with
+				product initiatives to bring in more business for the product.
+			</p>
+			<br />
+
+			<p>
+				Here's my
+				<a
+					href="https://www.linkedin.com/in/prakhar897/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					{" "}
+					LinkedIn{" "}
+				</a>
+				and
+				<a
+					href="https://prakgupta.com/Resume.pdf"
+					target="_blank"
+					rel="noreferrer"
+				>
+					{" "}
+					CV{" "}
+				</a>
+				to know more.
+			</p>
+			<br />
+			<p>
+				This site is a brain dump of everything I've learned from being
+				a software developer for the last 7 years. Check out the
+				<a href="./hireme"> Hire Me</a> page if you want to know more
+				about my work or to explore the possibility of working together.
+			</p>
+		</>
+	);
 };
 
 export default Home;
