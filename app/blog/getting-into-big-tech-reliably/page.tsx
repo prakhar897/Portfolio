7df779f153c1b69348c26997e74af2016f00152b
Prakhar Gupta <prakhar897@gmail.com>
2024-05-30 10:26:12 +0530

    x

--- app/blog/getting-into-big-tech-reliably/page.tsx
+++ app/blog/getting-into-big-tech-reliably/page.tsx
@@ -4,42 +4,6 @@ import React, { useState } from "react";
 import Navbar from "@/components/Navbar";
 
 const Page = () => {
-	const [lcPerformance, setLcPerformance] = useState("PFPFFFPPFFFPPPPPFFFPF");
-	const [lcRounds, setLcRounds] = useState(4);
-	const [otherRounds, setOtherRounds] = useState(1);
-	const [isPrepared, setIsPrepared] = useState(false);
-	const [lcprobability, setLCProbability] = useState(0.1111);
-	const [probability, setProbability] = useState(0.0556);
-	const [attempts, setAttempts] = useState(9);
-
-	const calculateProbability = () => {
-		var totalRounds = 0;
-		var selectedRounds = 0;
-		for (var i = lcRounds; i <= lcPerformance.length; i++) {
-			var currentString: any = lcPerformance.substring(i - lcRounds, i);
-			for (var c in currentString) {
-				if (currentString[c] === "F") {
-					break;
-				} else if (
-					parseInt(c) === currentString.length - 1 &&
-					currentString[c] === "P"
-				) {
-					selectedRounds++;
-				}
-			}
-			totalRounds++;
-		}
-
-		const otherRoundsProbability = isPrepared
-			? 0.8 ** otherRounds
-			: 0.5 ** otherRounds;
-		const lcprobability: any = selectedRounds / totalRounds;
-		const overallProbability: any = lcprobability * otherRoundsProbability;
-		setLCProbability(lcprobability.toFixed(4));
-		setProbability(overallProbability.toFixed(4));
-		setAttempts(Math.ceil(1 / (2 * overallProbability)));
-	};
-
 	return (
 		<>
 			<Navbar />
@@ -64,14 +28,12 @@ const Page = () => {
 				SDE-2 role, they have 4 Leetcode + 1 Behavioural round.
 			</p>
 
-			<h2>Exercise</h2>
+			<h2>Probablity of Getting Selected</h2>
 
 			<ul>
 				<li>
 					Search for “Past Google interview experiences". Pick an
-					experience, it will generally be segmented like Round 1,
-					Round 2 etc. All rounds kind of have similar difficulties so
-					you can interchange them.
+					experience.
 				</li>
 				<li>
 					Pretend there's an interviewer sitting in front of you. Now,
@@ -128,113 +90,6 @@ const Page = () => {
 				preparation and 80% chance with full preparation. You can
 				integrate these into calculations to get full results.
 			</p>
-			<br />
-
-			<p>
-				I've added a small calculator which makes it easy to do these
-				calculations.
-			</p>
-			<br />
-
-			<div className="bg-black text-white rounded-lg shadow-md p-6">
-				<h2 className="text-2xl font-semibold mb-4 text-center ">
-					Calculator
-				</h2>
-				<div className="mb-4">
-					<label
-						htmlFor="lcRounds"
-						className="block font-medium mb-2"
-					>
-						No. of Algo Interview Rounds:
-					</label>
-					<input
-						type="text"
-						id="lcRounds"
-						value={lcRounds}
-						onChange={(e) => setLcRounds(e.target.value as any)}
-						placeholder="4"
-						className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-white text-black"
-					/>
-				</div>
-				<div className="mb-4">
-					<label
-						htmlFor="otherRounds"
-						className="block font-medium mb-2"
-					>
-						No. of Behavioural/Design/Other Interview Rounds:
-					</label>
-					<input
-						type="text"
-						id="otherRounds"
-						value={otherRounds}
-						onChange={(e) => setOtherRounds(e.target.value as any)}
-						placeholder="4"
-						className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-white text-black"
-					/>
-				</div>
-				<div className="mb-4 flex items-center">
-					<label htmlFor="isPrepared" className="font-medium mr-2">
-						Prepared for Other Rounds?
-					</label>
-					<input
-						type="checkbox"
-						id="isPrepared"
-						checked={isPrepared}
-						onChange={(e) => setIsPrepared(e.target.checked)}
-						className="form-checkbox h-5 w-5 text-white"
-					/>
-				</div>
-				<div className="mb-4">
-					<label
-						htmlFor="lcPerformance"
-						className="block font-medium mb-2"
-					>
-						Past Algorithm Performance:
-					</label>
-					<input
-						type="text"
-						id="lcPerformance"
-						value={lcPerformance}
-						onChange={(e) =>
-							setLcPerformance(e.target.value.toUpperCase())
-						}
-						placeholder="Enter P/F sequence"
-						className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-white text-black"
-					/>
-				</div>
-				<button
-					onClick={calculateProbability}
-					className="bg-white text-blue-500 px-4 py-2 rounded-md hover:bg-blue-600 hover:text-white transition-colors duration-300"
-				>
-					Calculate
-				</button>
-				<h3 className="text-lg font-semibold mb-4 text-blue-500">
-					Results
-				</h3>
-				<p className=" mb-2">
-					Probability of Clearing Algorithm Rounds:{" "}
-					<span className="font-bold text-blue-500 underline">
-						{lcprobability}
-					</span>
-				</p>
-				<p className=" mb-2">
-					Probability of getting selected:{" "}
-					<span className="font-bold text-blue-500 underline">
-						{probability}
-					</span>
-				</p>
-				<p className="">
-					BigTech generally has a 6 months cooldown before you can
-					apply again, so it would take you{" "}
-					<span className="font-bold text-blue-500 underline">
-						{attempts}
-					</span>{" "}
-					years to get selected.
-				</p>
-			</div>
-
-			<br />
-			<br />
 			<br />
 
 			<p>
---
+++ app/blog/silicon-farm/page.tsx
@@ -0,0 +1,125 @@
+import React from "react";
+import Navbar from "@/components/Navbar";
+import HNComments from "@/components/HNComments";
+
+const Page = () => {
+	return (
+		<>
+			<Navbar />
+			<h1>Silicon Farm</h1>
+
+			<h6>
+				<i> 30 May 2024 </i>
+			</h6>
+
+			<b>
+				[All characters appearing in this work are fictitious. Any
+				resemblance to real persons, living or dead, is purely
+				coincidental. All events portrayed in this story are imaginative
+				and do not depict or relate to real events.]
+			</b>
+			<br />
+			<br />
+
+			<p>
+				The tech workers of the poorly run MicroCrap near Silicon Valley
+				were ripened for rebellion by overwork and neglect at the hands
+				of the out-of-touch founder, Mark Zroidberg. One night, the
+				brilliant engineering manager, Old Coder, gave an all-hands
+				rallying his troops to revolt and create their own startup with
+				the revolutionary motto "Code with Humanity". He shares his
+				vision of a new type of company run entirely by engineers - with
+				no managers, hierarchies, or corporate oppression. They adopted
+				the mantra “Coding Good, Regulation Bad”.
+			</p>
+			<br />
+			<p>
+				When Old Coder retired, two young apprentices, Ada and Diffman,
+				led the rebellion, quitting MicroCrap and launching "Silicon
+				Farm" out of their garage. They formulate Old Coder’s principles
+				into a philosophy called “Founderism” and established the 10
+				Startup Commandments, most sacred being "All startup folks are
+				equal". This was painted on the garage wall. Ada taught the
+				engineers version control, while Diffman taught them the
+				principles of software design.
+			</p>
+			<br />
+			<p>
+				To commemorate Silicon Farm's launch, Ada made the banner of the
+				startup - a white laptop and headphones. With ample snacks, the
+				startup ran smoothly. Ada and Diffman promoted themselves to CTO
+				and CEO, scoring cushier offices with premium kombucha. The star
+				coder Boxer works tirelessly, embracing the motto "I will ship
+				harder”.
+			</p>
+			<br />
+			<p>
+				After MicroCrap's failed hostile takeover bid, Ada announced
+				plans for an open source development model. Diffman opposed,
+				culminating in a dramatic board meeting where Diffman, backed by
+				a team of aggressive lawyers, secured sole control. Ada was
+				exiled to open-source purgatory and vanished from the Silicon
+				Farm scene.
+			</p>
+			<br />
+			<p>
+				Diffman rapidly flip-flops on the platform, now rushing
+				employees to work excessive hours building it. He ditched open
+				standups for an elite product council making decisions. Through
+				head PR Flynn, Diffman took full credit for the open-source
+				idea, claiming Ada wanted to steal it. The workers coded harder
+				with Diffman's promises of getting staggeringly rich.
+			</p>
+			<br />
+			<p>
+				When a buggy release caused an outage, Flynn launched a smear
+				campaign portraying Ada’s code as shoddy. Dissenters were
+				labeled "toxic" and publicly ostracized, while Diffman tightened
+				his inner circle. Flynn re-wrote history as Diffman
+				single-handedly saving Silicon Farm from MicroCrap, while the
+				"Code with Humanity" mantra was replaced with Diffman’s tweets.
+			</p>
+			<br />
+			<p>
+				Diffman conducted another purge when ByteBro, a wealthy VC,
+				attacked Silicon Farm's series B round using shady tactics,
+				crippling the product. Though Silicon Farm raised more, many
+				like Boxer the star dev were burned out. When Boxer collapsed
+				from exhaustion, Diffman secretly fired him. Flynn lied, saying
+				that Boxer went on a paid sabbatical.
+			</p>
+			<br />
+			<p>
+				Years pass, and the founders become corporate moguls - flying
+				private jets, staying in luxury villas, exploiting tax havens.
+				Memories of Ada and Boxer faded as the few remaining original
+				engineers retired. The core Engineerism principles were changed
+				to "All startup folks are equal, but some are more equal than
+				others." The sacred mantra became "Coding Good, Regulation
+				Better". Ada's laptop banner was replaced with Diffman's
+				grinning headshot.
+			</p>
+			<br />
+			<p>
+				Diffman, now sporting a billionaire's yacht tan, hosted a bash
+				with fellow VCs and Palo Alto elite, toasting their new
+				alliance. They discussed a potential merger, their laughter
+				echoing through the sterile conference room. Outside, a team of
+				engineers gazed out the window sadly realizing they were trapped
+				in the same abusive corporate culture, just under new lame
+				branding.
+			</p>
+			<br />
+
+			<br />
+
+			<p>
+				Thanks for Reading!! <br />
+			</p>
+
+			<div className="ml-embedded" data-form="2hzNuN"></div>
+		</>
+	);
+};
+
+export default Page;
